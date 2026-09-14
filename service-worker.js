ad074fb8574dd2a54445e2ea68de7fe8218aa317
noagrp <lihwlee11@gmail.com>
2026-09-15 02:07:38 +0800

    Refresh Tank Crawler cache for mobile shop fix

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='tankcrawler-v4';
+const CACHE='tankcrawler-v5';
 const ASSETS=['./','./index.html','./enhancements.js','./manifest.webmanifest','./favicon-32x32.png','./apple-touch-icon.png','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting()});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x)))));self.clients.claim()});
